adc6c65b518070fd7c18ee4f395835521d042d1d
Dmytro Vizantii <dmytrovizantii@gmail.com>
2021-10-18 20:32:00 +0300

    init commit

---
+++ src/com/pb/Vizantii/hw2/Calculator.java
@@ -0,0 +1,9 @@
+package com.pb.Vizantii.hw2;
+
+public class Calculator {
+    public static void main(String[] args) {
+       int x = 9;
+       int y = 0;
+       String sign = "+"; // + - * /
+    }
+}
---
+++ src/com/pb/Vizantii/hw2/Interval.java
@@ -0,0 +1,4 @@
+package com.pb.Vizantii.hw2;
+
+public class Interval {
+}
